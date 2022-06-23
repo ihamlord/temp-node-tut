fd97c91cac42926c97ec438609edf463121dd260
Adeleke Daniel <ihamlord@gmail.com>
2022-06-23 08:51:46 -0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+//CommonJS, every file is module in Node (by Default)
+// Modules - Encapsulated Code
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative')
+require('./7-mind-grenade')
+//sayHi('susan')
+//sayHi(names.john)
+//sayHi(names.peter)
